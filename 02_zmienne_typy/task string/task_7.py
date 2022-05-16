cecd8e689089ae135835539d88c0291e79d1e4e3
Rideek <rabinskid@rabdev.pl>
2022-05-16 09:29:51 +0200

    add task 6 and 7 to task string file

---
+++ 02_zmienne_typy/task string/task_7.py
@@ -0,0 +1,8 @@
+# -- TASK7 -- Stwórz dwie dowolne zmienne przechowujące wartość liczbową i tekstową. Użyj funkcji format(), by wyświetlić zdanie zawierające te wartości.
+
+number = 12312312312312
+txt = 'hakunamatata'
+
+new_txt ='{}'.format(txt+str(number)+)
+print(new_txt)
+
